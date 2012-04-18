61bfcf91c7331f60df793f2850a6e4bf1c88bb8b
Rasmus Hummelmose <rasmus@hummelmose.dk>
2012-04-18 23:56:57 +0200

    Initial Commit

---
+++ EPay/EPayTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EPayTestAppDelegate.h
+//  EPay
+//
+//  Created by Rasmus Hummelmose on 18/04/12.
+//  Copyright (c) 2012 R&S I/S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPayTestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EPay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EPay
+//
+//  Created by Rasmus Hummelmose on 18/04/12.
+//  Copyright (c) 2012 R&S I/S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EPayTestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EPayTestAppDelegate class]));
+    }
+}
